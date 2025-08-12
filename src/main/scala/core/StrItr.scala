5ee9e0228da69f68092d6576664e485504efbfe8
Amin Malekloo <amin.malekloo@persgroep.net>
2025-08-12 12:47:10 +0200

    remove unnecessary paren for accessor-like method 'hasNext'

--- src/main/scala/core/StrItr.scala
+++ src/main/scala/core/StrItr.scala
@@ -5,11 +5,11 @@ import scala.annotation.tailrec
 
 class StrItr(val raw: String, var pos: Int) {
   private final val len           = raw.length
-  inline def peek(): Option[Char] = if hasNext() then Some(raw(pos)) else None
+  inline def peek(): Option[Char] = if hasNext then Some(raw(pos)) else None
 
   inline def take(n: Int): String = raw.slice(pos, pos + n)
 
-  inline def hasNext(): Boolean = pos < len
+  inline def hasNext: Boolean = pos < len
 
   def takeWhile(p: Char => Boolean): String = {
     @tailrec def matchCountrTR(matchCount: Int = 0): Int =
