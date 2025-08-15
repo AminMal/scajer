99246125e076f6274434e88850c49fb6a7af0d98
Amin Malekloo <amin.malekloo@persgroep.net>
2025-08-15 23:03:18 +0200

    remove external libraries

--- build.sbt
+++ build.sbt
@@ -7,12 +7,5 @@ lazy val root = (project in file("."))
   .settings(
     name := "Scajer",
     idePackagePrefix := Some("com.github.aminmal"),
-    libraryDependencies ++= Seq(
-      "io.circe" %% "circe-core"    % "0.14.6",
-      "io.circe" %% "circe-parser"  % "0.14.6",
-      "io.circe" %% "circe-generic" % "0.14.6",
-      "com.typesafe.play" %% "play-json" % "2.10.7",
-      "com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-core"   % "2.37.4"
-    ),
     scalafmtOnCompile := true
   )
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,18 +6,5 @@ import core.{Parser, StrItr}
 
 object Main extends App {
   val src = Source.fromResource("input.json").mkString
-
-  def bench[T](n: Int, op: () => T)(tag: String): Long = {
-    val start = System.currentTimeMillis()
-    (0 until n).foreach(_ => op())
-    val end   = System.currentTimeMillis()
-    end - start
-  }
-
-  val mine   = bench(70000, () => Parser.parse(src))("my_parser")
-  val circes = bench(70000, () => io.circe.parser.parse(src))("circe")
-  val plays  = bench(70000, () => play.api.libs.json.Json.parse(src))("play")
-
-  println(s"mine took $mine ms, circe took $circes ms, play took $plays ms")
-//  println(Parser.parse(src).toOption.get.prettyPrint())
+  println(Parser.parse(src).toOption.get.prettyPrint())
 }
